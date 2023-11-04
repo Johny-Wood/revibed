35a6ccea00be3838169c3e06ee3e8d2b04dabc88
Johny-Wood <91822495+Johny-Wood@users.noreply.github.com>
2023-11-04 18:15:52 +0300

    fix: fix find music input redirection

--- src/js/pages/main-projects/MainPageNotAuthorizedWrapper/_components/MainCards/index.jsx
+++ src/js/pages/main-projects/MainPageNotAuthorizedWrapper/_components/MainCards/index.jsx
@@ -1,23 +1,136 @@
-import Image from 'next/image';
-import { useRouter } from 'next/navigation'
+import { useCallback, useMemo, useRef, useState } from 'react';
 
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { connect } from 'react-redux';
 
 import SiteWrapperLayout from '@/components/layouts/SiteWrapperLayout';
 import SearchInput from '@/js/components/common-ui/inputs/SearchInput/';
 import Button from '@/js/components/ui/buttons/Button';
+import { MarketplaceLocationsConstants } from '@/js/constants/marketplace/location';
+// import { ProjectsLocationsConstants } from '@/js/constants/projects/location';
+import { getMarketplaceListRequestAction, setMarketplaceSearchAction } from '@/js/redux/actions/marketplace/marketplaceActions';
 
 import styles from './styles.module.scss';
 
-function MainCards({ events }) {
-    const router = useRouter()
+function MainCards({ events, getProjects, getMarketplaceListRequest, setMarketplaceSearch }) {
+  const router = useRouter();
+
+  // const searchResultsLimit = 5;
+  // const querySearchCurrent = useRef('');
+  // const querySearchPrev = useRef(querySearchCurrent.current);
+
+  // const [location, setLocation] = useState(0);
+  //
+  // const [marketplaceListStatus, setMarketplaceListStatus] = useState('idle');
+  // const [preOrdersListStatus, setPreOrdersListStatus] = useState('idle');
+  //
+  // const [marketplaceList, setMarketplaceList] = useState([]);
+  // const [marketplaceListCount, setMarketplaceListCount] = useState(0);
+  // const [preOrdersList, setPreOrdersList] = useState([]);
+  // const [preOrdersListCount, setPreOrdersListCount] = useState(0);
+
+  // const searchConfig = useMemo(
+  //   () => ({
+  //     pageNumber: 0,
+  //     pageSize: searchResultsLimit,
+  //     withInProcess: false,
+  //     withSort: false,
+  //     withFilters: false,
+  //     useCustomResponseHandler: true,
+  //   }),
+  //   []
+  // );
+  //
+  // const changeSearch = useCallback(
+  //   (querySearch = '', newLocation = location, isChangeLocation) =>
+  //     new Promise((resolve, reject) => {
+  //       if (isChangeLocation && querySearchPrev.current === querySearchCurrent.current) {
+  //         resolve(null);
+  //
+  //         return;
+  //       }
+  //
+  //       if (newLocation === 0) {
+  //         setMarketplaceListStatus('inProcess');
+  //
+  //         getMarketplaceListRequest({
+  //           ...searchConfig,
+  //           location: MarketplaceLocationsConstants.MARKETPLACE,
+  //           querySearch,
+  //         })
+  //           .then(
+  //             ({
+  //               responseData,
+  //               payload: {
+  //                 page: { totalElements = 0 },
+  //               },
+  //             }) => {
+  //               setMarketplaceList(responseData);
+  //               setMarketplaceListCount(totalElements);
+  //               setMarketplaceSearch({ search: querySearch, location: MarketplaceLocationsConstants.MARKETPLACE });
+  //               router.push({
+  //                 pathname: '/marketplace',
+  //                 query: { query: querySearch},
+  //               });
+  //
+  //
+  //
+  //
+  //               resolve(null);
+  //             }
+  //           )
+  //           .catch(reject)
+  //           .finally(() => {
+  //             setMarketplaceListStatus(querySearch ? 'success' : 'idle');
+  //
+  //             querySearchCurrent.current = querySearch;
+  //           });
+  //       } else if (newLocation === 1) {
+  //         setPreOrdersListStatus('inProcess');
+  //
+  //         getProjects({
+  //           ...searchConfig,
+  //           location: ProjectsLocationsConstants.PROJECTS,
+  //           querySearch,
+  //         })
+  //           .then(
+  //             ({
+  //               responseData,
+  //               payload: {
+  //                 page: { totalElements = 0 },
+  //               },
+  //             }) => {
+  //               setPreOrdersList(responseData);
+  //               setPreOrdersListCount(totalElements);
+  //
+  //               resolve(null);
+  //             }
+  //           )
+  //           .catch(reject)
+  //           .finally(() => {
+  //             setPreOrdersListStatus(querySearch ? 'success' : 'idle');
+  //
+  //             querySearchCurrent.current = querySearch;
+  //           });
+  //
+  //         resolve(null);
+  //       }
+  //     }),
+  //   [getMarketplaceListRequest, getProjects, location, searchConfig]
+  // );
 
   const onSearch = (value) => {
+    setMarketplaceSearch({ search: value, location: MarketplaceLocationsConstants.MARKETPLACE });
     router.push({
-      pathname: "/marketplace",
-      query: {query: value}
-    })
-  }
+      pathname: '/marketplace',
+      query: { query: value},
+    });
+    // router.push({
+    //   pathname: '/marketplace',
+    //   query: { query: value },
+    // });
+  };
 
   return (
     <SiteWrapperLayout direction="column" className={styles.MainCards}>
@@ -25,6 +138,8 @@ function MainCards({ events }) {
         <SearchInput
           id="MainLandingSearch"
           onSearch={onSearch}
+          // onSearch={changeSearch}
+          // searched={marketplaceListStatus !== 'idle' || preOrdersListStatus !== 'idle'}
           border={false}
           className={styles.MainCards__input}
           size="large"
@@ -33,7 +148,7 @@ function MainCards({ events }) {
         <Button className={styles.MainCards__button}>Find music</Button>
       </div>
       <div className={styles.MainCards__items}>
-        {events.slice(0, 5).map((card) => {
+        {events?.slice(0, 5).map((card) => {
           const artists = card.release.artists.map((item) => item.name).join(', ');
 
           return (
@@ -44,18 +159,44 @@ function MainCards({ events }) {
               <div className={styles.MainCard__info}>
                 <div className={styles.MainCard__infoTitle}>{artists || ''}</div>
                 <div className={styles.MainCard__infoDesc}>{card.name}</div>
-                <Button className={styles.MainCard__button} onClick={() => router.push("/marketplace/" + card.id)}>Buy now</Button>
+                <Button className={styles.MainCard__button} onClick={() => router.push('/marketplace/' + card.id)}>
+                  Buy now
+                </Button>
               </div>
             </div>
           );
         })}
       </div>
-      <Button className={styles.MainCards__more} onClick={() => router.push("/marketplace")}>Find More</Button>
+      <Button className={styles.MainCards__more} onClick={() => router.push('/marketplace')}>
+        Find More
+      </Button>
     </SiteWrapperLayout>
   );
 }
 
-export default connect((state) => ({
-  events: state.MarketplaceNewReleasesListReducer.list,
-  getMarketplaceListFromApi: state.MarketplaceNewReleasesListReducer.getMarketplaceListFromApi,
-}))(MainCards);
+const connector = connect(
+  (state) => ({
+    events: state.MarketplaceNewReleasesListReducer.list,
+    getMarketplaceListFromApi: state.MarketplaceNewReleasesListReducer.getMarketplaceListFromApi,
+  }),
+  (dispatch) => ({
+    // getProjects: (params) => getProjectsRequestAction(params)(dispatch),
+    // getMarketplaceListRequest: (params) =>
+    //   getMarketplaceListRequestAction({
+    //     ...params,
+    //     dispatch,
+    //   }),
+    setMarketplaceSearch: (params) => {
+      dispatch(setMarketplaceSearchAction(params));
+    },
+  })
+);
+
+export default connector(MainCards);
+// export default connect((state, dispatch) => ({
+//   events: state.MarketplaceNewReleasesListReducer.list,
+//   getMarketplaceListFromApi: state.MarketplaceNewReleasesListReducer.getMarketplaceListFromApi,
+//   setMarketplaceSearch: (params) => {
+//     dispatch(setMarketplaceSearchAction(params));
+//   },
+// }))(MainCards);
--- src/js/pages/main-projects/MainPageNotAuthorizedWrapper/index.jsx
+++ src/js/pages/main-projects/MainPageNotAuthorizedWrapper/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import BaseWebsiteLayout from '@/components/layouts/BaseWebsiteLayout';
 import Footer from '@/js/components/global/Footer';
@@ -9,12 +9,15 @@ import FanPovered from './_components/FanPovered';
 import MainCards from './_components/MainCards';
 import Preorders from './_components/Preorders';
 
+import styles from './index.module.scss'
+
 const metaTitle = 'Wanted Vinyl Records, CDs & Cassette Tapes In High Resolution';
 const metaDescription =
   'Rediscover some of the most forgotten and wanted music records of all time and support the original artists. Welcome to the most innovative tool to collect rare music.';
 
 function MainPageNotAuthorizedWrapper({ userIsAuthorized, isNotDesktop }) {
   const [scrollValue, setScrollValue] = useState(0);
+
 
   return (
     <BaseWebsiteLayout
@@ -31,12 +34,26 @@ function MainPageNotAuthorizedWrapper({ userIsAuthorized, isNotDesktop }) {
       onScrollHandler={setScrollValue}
       withoutFooter
     >
-      <MainBanner scrollValue={scrollValue.scrollTop} />
-      <MainCards />
-      <FanPovered />
-      <Preorders />
-      <Collector scrollObject={scrollValue} />
-      <Footer footerProps={{ blackFooter: true }} />
+      <div className={styles.cont}>
+        <div className={styles.cont__child}>
+          <MainBanner scrollValue={scrollValue.scrollTop} />
+        </div>
+        <div className={styles.cont__child}>
+          <MainCards />
+        </div>
+        <div className={styles.cont__child}>
+          <FanPovered />
+        </div>
+        <div className={styles.cont__child}>
+          <Preorders />
+        </div>
+        <div className={styles.cont__child}>
+          <Collector scrollObject={scrollValue} />
+        </div>
+        <div className={styles.cont__child}>
+          <Footer footerProps={{ blackFooter: true }} />
+        </div>
+      </div>
     </BaseWebsiteLayout>
   );
 }
